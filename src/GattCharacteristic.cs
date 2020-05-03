207a8d3243222c874dfd8e2efd526aacf8e9b919
Yaakov <yaakov-h@users.noreply.github.com>
2020-05-03 17:54:52 +1000

    Remove console output when GattCharacteristic gets disposed or finalized

--- src/GattCharacteristic.cs
+++ src/GattCharacteristic.cs
@@ -31,7 +31,6 @@ namespace HashtagChris.DotNetBlueZ
 
     public void Dispose()
     {
-      Console.WriteLine("GattCharacteristic disposing.");
       m_propertyWatcher?.Dispose();
       m_propertyWatcher = null;
 
